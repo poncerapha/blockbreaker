d6ab2b56dff7f8e8ff761f94cf83087c801d107e
Raphael Ponce <raphaelponce@MacBook-Pro-de-Raphael.local>
2019-09-14 23:01:57 -0300

    Implementando Singleton

--- blockbreaker/Assets/Scripts/Block.cs
+++ blockbreaker/Assets/Scripts/Block.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    [SerializeField] AudioClip breakSound;
+  [SerializeField] AudioClip breakSound;
 
-    LevelManager levelManager;
+  LevelManager levelManager;
+
+  GameManager gameManager;
 
 
-    private void Start()
-    {
-        levelManager = FindObjectOfType<LevelManager>();
-        levelManager.CountBlocks();
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Collision!!!");
-        DestroyBlock();
+  private void Start()
+  {
+    levelManager = FindObjectOfType<LevelManager>();
+    gameManager = FindObjectOfType<GameManager>();
+    levelManager.CountBlocks();
+  }
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    Debug.Log("Collision!!!");
+    gameManager.AddToScore();
+    DestroyBlock();
 
-    }
+  }
 
-    private void DestroyBlock()
-    {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        Destroy(this.gameObject);
-        levelManager.BlockDestroyed();
-    }
+  private void DestroyBlock()
+  {
+    AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    Destroy(this.gameObject);
+    levelManager.BlockDestroyed();
+  }
 }
---
+++ blockbreaker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+  [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+  [SerializeField] int pointsPerBlock = 20;
+  [SerializeField] TextMeshProUGUI scoreText;
+
+  [SerializeField] int currentScore = 0;
+  // Start is called before the first frame update
+
+  void Awake()
+  {
+    int gameManagerCount = FindObjectsOfType<GameManager>().Length;
+    if (gameManagerCount > 1)
+    {
+      gameObject.SetActive(false);
+      Destroy(gameObject);
+    }
+    else
+    {
+      DontDestroyOnLoad(gameObject);
+    }
+  }
+  void Start()
+  {
+    scoreText.text = currentScore.ToString();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Time.timeScale = gameSpeed;
+
+  }
+
+  public void AddToScore()
+  {
+    currentScore += pointsPerBlock;
+    scoreText.text = currentScore.ToString();
+  }
+
+
+
+
+}
