a4863f88e98957908715fc0b6a272405514c01f2
Raphael Ponce <raphaelponce@MacBook-Pro-de-Raphael.local>
2019-09-15 02:02:03 -0300

    Implementando 2 hits blocs e 3 hits blocks

--- blockbreaker/Assets/Scripts/Ball.cs
+++ blockbreaker/Assets/Scripts/Ball.cs
@@ -6,63 +6,72 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
-    [SerializeField] Paddle paddle1;
-    [SerializeField] float xPush = 2f;
-    [SerializeField] float yPush = 15f;
-    [SerializeField] AudioClip[] ballSounds;
+  [SerializeField] Paddle paddle1;
+  [SerializeField] float xPush = 2f;
+  [SerializeField] float yPush = 15f;
+  [SerializeField] AudioClip[] ballSounds;
+
+  [SerializeField] float randomFactor = 0.5f;
 
 
-    Vector2 paddleToBallVector;
+  Vector2 paddleToBallVector;
 
-    private bool isBauncing = false;
+  private bool isBauncing = false;
 
 
-    AudioSource audioSource;
+  AudioSource audioSource;
 
-    // Start is called before the first frame update
-    void Start()
+  Rigidbody2D rigidbody2D;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    audioSource = GetComponent<AudioSource>();
+    rigidbody2D = GetComponent<Rigidbody2D>();
+    paddleToBallVector = transform.position - paddle1.transform.position;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    LockBallToPaddle();
+    LauchOnMouseClick();
+
+  }
+
+  private void LauchOnMouseClick()
+  {
+    if (Input.GetMouseButtonDown(0) && isBauncing == false)
     {
-        audioSource = GetComponent<AudioSource>();
-        paddleToBallVector = transform.position - paddle1.transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        LockBallToPaddle();
-        LauchOnMouseClick();
-
-    }
-
-    private void LauchOnMouseClick()
-    {
-        if (Input.GetMouseButtonDown(0) && isBauncing == false)
-        {
-            Debug.Log("Launching the ball");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
-            isBauncing = true;
-
-        }
-    }
-
-    private void LockBallToPaddle()
-    {
-        if (!isBauncing)
-        {
-            Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
-            transform.position = paddlePos + paddleToBallVector;
-        }
+      Debug.Log("Launching the ball");
+      rigidbody2D.velocity = new Vector2(xPush, yPush);
+      isBauncing = true;
 
     }
+  }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+  private void LockBallToPaddle()
+  {
+    if (!isBauncing)
     {
-        if (isBauncing)
-        {
-            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
-            audioSource.PlayOneShot(clip);
+      Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+      transform.position = paddlePos + paddleToBallVector;
+    }
 
-        }
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    Vector2 velocityTweak = new Vector2(UnityEngine.Random.Range(0f, randomFactor), UnityEngine.Random.Range(0f, randomFactor));
+    if (isBauncing)
+    {
+      AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+      audioSource.PlayOneShot(clip);
+      rigidbody2D.velocity += velocityTweak;
+
+
 
     }
+
+  }
 }
--- blockbreaker/Assets/Scripts/Block.cs
+++ blockbreaker/Assets/Scripts/Block.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,29 +7,80 @@ public class Block : MonoBehaviour
 {
   [SerializeField] AudioClip breakSound;
 
+  [SerializeField] int currentHits;
+  [SerializeField] Sprite[] hitSprites;
+
   LevelManager levelManager;
 
   GameManager gameManager;
+
+  private GameObject particle;
 
 
   private void Start()
   {
     levelManager = FindObjectOfType<LevelManager>();
     gameManager = FindObjectOfType<GameManager>();
-    levelManager.CountBlocks();
+    if (this.tag == "Breakable")
+    {
+      levelManager.CountBlocks();
+
+    }
   }
   private void OnCollisionEnter2D(Collision2D collision)
   {
-    Debug.Log("Collision!!!");
-    gameManager.AddToScore();
-    DestroyBlock();
+    if (this.tag == "Breakable")
+    {
+      Debug.Log("Collision!!!");
+      HandleHit();
 
+    }
+
+  }
+
+  private void HandleHit()
+  {
+    currentHits++;
+    int maxHits = hitSprites.Length + 1;
+    if (currentHits >= maxHits)
+    {
+
+      DestroyBlock();
+
+    }
+    else
+    {
+      ShowNextHitSprite();
+    }
+  }
+
+  private void ShowNextHitSprite()
+  {
+    int spriteIndex = currentHits - 1;
+    if (spriteIndex != null)
+    {
+      SpriteRenderer spriteRenderer = this.GetComponent<SpriteRenderer>();
+      spriteRenderer.sprite = hitSprites[spriteIndex];
+
+    }
+    else
+    {
+      Debug.LogError("Block sprite is missing from array, the gameobject: " + this.gameObject.name);
+    }
   }
 
   private void DestroyBlock()
   {
     AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    gameManager.AddToScore();
     Destroy(this.gameObject);
     levelManager.BlockDestroyed();
+    // TriggerParticle();
+  }
+
+  private void TriggerParticle()
+  {
+    GameObject particles = Instantiate(particle, transform.position, transform.rotation);
+    Destroy(particles, 1f);
   }
 }
